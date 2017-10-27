02186156d8135823d03d3653827fd37560878182
kimlan nguyen <kimlan1510@yahoo.com>
2017-10-26 21:45:56 -0700

    Create boomerang, add background image

---
+++ Assets/Scripts/Motion/BoomerangFly.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace boomerang.motion
+{
+    public class BoomerangFly : MonoBehaviour
+    {
+        private Rigidbody g_rigid_body;
+        public float g_fly_speed;
+
+	    // Use this for initialization
+	    void Start ()
+        {
+            g_rigid_body = gameObject.GetComponent<Rigidbody> ();
+            g_rigid_body.velocity = transform.forward * g_fly_speed;
+	    }
+	
+	    // Update is called once per frame
+	    void Update ()
+        {
+		
+	    }
+    }
+
+}
