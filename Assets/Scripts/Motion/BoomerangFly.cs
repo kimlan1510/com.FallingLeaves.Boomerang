af78c5265c5a4ca8806741aef4bb8e616fd9bc38
kimlan nguyen <kimlan1510@yahoo.com>
2017-10-26 21:58:32 -0700

    add spin to boomerang

--- Assets/Scripts/Motion/BoomerangFly.cs
+++ Assets/Scripts/Motion/BoomerangFly.cs
@@ -8,12 +8,15 @@ namespace boomerang.motion
     {
         private Rigidbody g_rigid_body;
         public float g_fly_speed;
+        public float g_spin;
 
 	    // Use this for initialization
 	    void Start ()
         {
             g_rigid_body = gameObject.GetComponent<Rigidbody> ();
             g_rigid_body.velocity = transform.forward * g_fly_speed;
+            g_rigid_body.angularVelocity = new Vector3 (0f, 0f, g_spin);
+
 	    }
 	
 	    // Update is called once per frame
